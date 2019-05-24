108f15a47fdaa3da0ded702fa44aa95adde81877
CharileHuang <charile@apple,pro>
2019-05-24 10:34:57 +0800

    Add tt1 UpDate
    測試新增參數

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var testWebView: WKWebView!
     
+    let tt1 = 0
+    
     let DarkGrayColor: UIColor = UIColor(red: 43/255, green: 43/255, blue: 43/255, alpha: 1)
     
 //    let webHTML = try! String(contentsOfFile: Bundle.main.path(forResource: "testScript", ofType: "html")!, encoding: String.Encoding.utf8)
